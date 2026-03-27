453dd2d6f0fcab4ec5520578b79c6ef0e847e9c3
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-03-27 18:08:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ if st.button("🚀 Generate Voice"):
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
             loop.run_until_complete(
-                emotion = emotion_map[emotion_name]
+emotion = emotion_map[emotion_name]
 
 generate_voice(
     final_text,
