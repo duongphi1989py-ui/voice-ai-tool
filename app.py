5c86e441745cecfeae8bf0c8fe8abb0020b8f2a0
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-03-27 17:59:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,22 @@ def story_engine(text, cfg):
         return " ".join(out)
 
     return text
+def split_text(text, max_length=300):
+    sentences = re.split(r'(?<=[.!?]) +', text)
+    chunks = []
+    current = ""
 
+    for s in sentences:
+        if len(current) + len(s) < max_length:
+            current += " " + s
+        else:
+            chunks.append(current.strip())
+            current = s
+
+    if current:
+        chunks.append(current.strip())
+
+    return chunks
 # ================= CONFIG =================
 cfg = {
     "dot": pause_dot,
