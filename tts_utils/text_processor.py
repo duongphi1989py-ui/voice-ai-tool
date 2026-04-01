391f41e652dcb2f1c511e295fb409a5b3224f11e
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-04-01 11:09:20 +0700

    Update text_processor.py

--- tts_utils/text_processor.py
+++ tts_utils/text_processor.py
@@ -1,19 +1,12 @@
 import re
 
 def process_text(text: str) -> str:
-    """
-    Xử lý văn bản để thêm pause theo dấu câu
-    """
-
-    # Chuẩn hóa xuống dòng
     text = re.sub(r'\n+', '\n', text)
 
-    # Thêm pause theo dấu
-    text = text.replace(",", ", <break time='200ms'/>")
-    text = text.replace(".", ". <break time='100ms'/>")
-    text = text.replace(":", ": <break time='100ms'/>")
+    text = text.replace(",", ", ...")
+    text = text.replace(".", ". ...")
+    text = text.replace(":", ": ...")
 
-    # Xuống dòng -> nghỉ lâu hơn
-    text = text.replace("\n", "\n<break time='300ms'/>")
+    text = text.replace("\n", "\n... ")
 
     return text
