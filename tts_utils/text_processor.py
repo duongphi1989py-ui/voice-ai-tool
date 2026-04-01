a40ef04e2c71dffa2b0592e41dc8ddd3443d40d3
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-04-01 11:21:26 +0700

    Update text_processor.py

--- tts_utils/text_processor.py
+++ tts_utils/text_processor.py
@@ -4,14 +4,14 @@ def process_text(text: str) -> str:
     text = re.sub(r'\n+', '\n', text)
 
     text = text.replace(",", ", ")
-    text = text.replace(".", ". ..")
+    text = text.replace(".", ". .")   # nghỉ nhẹ
     text = text.replace(":", ": ")
     text = text.replace("!", "! ")
-    text = text.replace(""", "" ")
+    text = text.replace('"', '" ')
     text = text.replace("?", "? ")
     text = text.replace("[", "[ ")
     text = text.replace("]", "] ")
 
-    text = text.replace("\n", "\n..")
+    text = text.replace("\n", "\n. ")  # xuống dòng nghỉ vừa
 
     return text
