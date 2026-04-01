34da6ae9efcda8196f324a404e277176ba6f6b08
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-04-01 11:14:15 +0700

    Update text_processor.py

--- tts_utils/text_processor.py
+++ tts_utils/text_processor.py
@@ -3,10 +3,10 @@ import re
 def process_text(text: str) -> str:
     text = re.sub(r'\n+', '\n', text)
 
-    text = text.replace(",", ", ...")
-    text = text.replace(".", ". ...")
-    text = text.replace(":", ": ...")
+    text = text.replace(",", ", ")
+    text = text.replace(".", ". .")
+    text = text.replace(":", ": ")
 
-    text = text.replace("\n", "\n... ")
+    text = text.replace("\n", "\n.")
 
     return text
