53f0ca4647ede20f6c639e6cfd6cd4965fc829fd
duongphi1989py-ui <duongphi1989py@gmail.com>
2026-04-02 13:39:15 +0700

    Update text_processor.py

--- tts_utils/text_processor.py
+++ tts_utils/text_processor.py
@@ -74,7 +74,7 @@ def process_text(text: str) -> str:
     text = text.replace("?", ", ")
 
     # bỏ dấu " gây khựng → đổi thành ,
-    text = text.replace('"', ', ')
+    text = text.replace('"', '')
 
     return text
 
